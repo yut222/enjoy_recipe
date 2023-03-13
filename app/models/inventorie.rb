b127b705a6ebf3aa97ab464dfdedf25bddd7fbaa
EC2 Default User <ec2-user@ip-172-31-6-238.ap-northeast-1.compute.internal>
2023-03-13 13:21:08 +0000

    [Add] inventroriesモデルカラム作成

---
+++ app/models/inventorie.rb
@@ -0,0 +1,2 @@
+class Inventorie < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_13_112909) do
+ActiveRecord::Schema.define(version: 2023_03_13_115457) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -38,6 +38,17 @@ ActiveRecord::Schema.define(version: 2023_03_13_112909) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "inventories", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "quantity", null: false
+    t.date "expiration_date", null: false
+    t.text "memo"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_inventories_on_user_id"
   end
 
   create_table "relationships", force: :cascade do |t|
@@ -81,4 +92,5 @@ ActiveRecord::Schema.define(version: 2023_03_13_112909) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "inventories", "users"
 end
