45228533ccb6e716f6deb95bff0bf47e5bdaace8
EC2 Default User <ec2-user@ip-172-31-6-238.ap-northeast-1.compute.internal>
2023-03-14 12:16:11 +0000

    [Add] comments/favoritesコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
 
+
   root 'homes#index'
+
+
+  resources :recipes do
+    resources :comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
